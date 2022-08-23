5c34cf4108acff39ba89ceb58e141ae08868d773
raineepei <pei.rainee@gmail.com>
2022-08-22 17:29:51 -0700

    updated navbar

--- components/Footer.js
+++ components/Footer.js
@@ -10,12 +10,12 @@ const Footer = () => {
         <nav className={styles.footerbar}>
             <ul className={styles.footermenu}>
                 <li className={styles.footeritemfirst}>
-                    <Link href='/'>
+                    <Link href="mailto: pei.rainee@gmail.com">
                         <a className={styles.footerlink}><u>EMAIL</u></a>
                     </Link>
                 </li>
                 <li className={styles.footeritem}>
-                    <Link href='/'>
+                    <Link href='https://www.linkedin.com/in/raineepei/'>
                         <a className={styles.footerlink}><u>LINKEDIN</u></a>
                     </Link>
                 </li>
@@ -26,7 +26,7 @@ const Footer = () => {
                 </li>
             </ul>
             <li className={styles.footeritemlast}>
-                <Link href='/'>
+                <Link href='https://github.com/raineepei'>
                     <a className={styles.footerlink}>Made with <span className={styles.footerheart}><b>&lt;3</b></span> by Rainee</a>
                 </Link>
             </li>
