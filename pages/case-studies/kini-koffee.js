708061cafce3803d2d46fd5679c5c9dd75356399
raineepei <pei.rainee@gmail.com>
2022-09-02 15:27:43 -0700

    kini koffee first section

--- pages/case-studies/kini-koffee.js
+++ pages/case-studies/kini-koffee.js
@@ -36,7 +36,25 @@ export default function KiniKoffee() {
 
             <div>
                 <h3 className={styles.sectiontitle}>OVERVIEW</h3>
+                <p className={styles.sectiontext}>Located in Del Mar and Encinitas (San Diego), Kini Koffee is a female-owned coffee shop 
+                featuring unique and creative drinks. Its aim is to build a community of women who work unconventional jobs 
+                while also creating a fun place for people to grab a quick drink on their way to the nearby beaches.</p>
             </div>
+
+            <div>
+                <h3 className={styles.sectiontitle}>ROLE</h3>
+                <p className={styles.sectiontext}>As the team’s UX Researcher, I spearheaded the research and interview process, 
+                including the end-to-end process, which involved recruitment and synthesis. In collaboration with the UI/UX 
+                and Visual Designers, I helped pull key insights from our research and interviews to help inform our design decisions. 
+                The work showcased below are my <span className={styles.bolded}>primary contributions.</span></p>
+            </div>
+
+            <div className={styles.buttoncontainer}>
+                <a className={styles.sitebutton}>CHECK OUT THE SITE!</a>
+            </div>
+
+            {/* horizontal linebreak */}
+            <header className={styles.breakline}></header>
 
         </div>
     );
